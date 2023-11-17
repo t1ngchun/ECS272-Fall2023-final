8afbf5e2a380b6ff3706e38ea7093f4160877feb
CTingChun <claire.chen@ui.com>
2023-11-16 17:54:01 -0800

    implement scroller

---
+++ clrchen/src/App.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Timeline from './components/Timeline.vue'
+
+export default {
+  data() {
+    return {
+      filter: []
+    };
+  },
+  components: {
+    Timeline,
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!--This is using the grid component from Vuetify to do layout design-->
+<template>
+  <v-container id="main-container" class="d-flex flex-nowrap" fluid>
+    <Timeline/>
+  </v-container>
+</template>
+
+<style scoped>
+#main-container{
+  height: 100%;
+}
+</style>
